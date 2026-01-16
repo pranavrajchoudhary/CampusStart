a053fb8275ac58b7807bcf5b5d773c163def7cac
Pranav Choudhary <choudharypranav128@gmail.com>
2026-01-16 18:46:47 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { registerUser, loginUser, getProfile } = require("../controllers/authController");
 const { protect } = require("../middlewares/authMiddleware");
 
-// Routes
+ 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-// router.get("/profile", protect, getProfile);
+ 
 
 module.exports = router;
