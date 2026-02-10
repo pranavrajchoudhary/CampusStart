b077f496bd198b6802644193a97103dc1733cbaf
Pranav Choudhary <choudharypranav128@gmail.com>
2026-02-10 17:20:24 +0530

    Update axiosConfig.js
    Remove fallback URL for API base URL in axios config.

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
-// Add token automatically if available
+
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
