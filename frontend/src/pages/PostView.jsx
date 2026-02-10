1800afd712bd06a08d44b4d1ce929f21d40638ad
Pranav Choudhary <choudharypranav128@gmail.com>
2026-02-10 18:01:48 +0530

    Update PostView.jsx

--- frontend/src/pages/PostView.jsx
+++ frontend/src/pages/PostView.jsx
@@ -1,4 +1,4 @@
-// src/pages/PostView.jsx
+ 
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Container, Box, CircularProgress, Typography } from "@mui/material";
